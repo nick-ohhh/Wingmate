1fe9be2e4708471abb46637e058d25233eb305c3
Rmolimock <495@holbertonschool.com>
2019-10-15 23:25:53 +0000

    django proj with pages app that serves the web static upon request to root. Integration with gunicorn and nginx still to come.

---
+++ Wingmates_Views/web_app/mvp/wingmate/pages/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+admin.site.site_header = "Wingmate Admin Panel"
+admin.site.site_title = "Yeaaa thaattss riighhttt"
+admin.site.index_title = "Wingmate!"  
