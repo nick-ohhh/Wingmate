8376d22d1a0e9dfd1232f280f4d94e5ee4bbcf04
Rmolimock <495@holbertonschool.com>
2019-10-19 07:42:31 +0000

    new route 'search' which allows passing location to the back end. Still working on integrating with jquery/generate button

--- web_app/mvp/wingmate/pages/views.py
+++ web_app/mvp/wingmate/pages/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+
+def search(request, location):
+    context = {'location': location}
+    return render(request, 'pages/index.html', context, content_type='string')
