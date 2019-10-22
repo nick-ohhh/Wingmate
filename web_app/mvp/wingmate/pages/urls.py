db2e157001c6bd1333e65e20b6cf5fb3e8477e9b
root <root@wingmate-vm.us-central1-a.c.wingmates.internal>
2019-10-22 21:11:30 +0000

    adding time for search criteria

--- web_app/mvp/wingmate/pages/urls.py
+++ web_app/mvp/wingmate/pages/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('search/<location>/', views.search, name="search"),
+    path('search/<location>/<date>/<time>', views.full_search, name="full_search"),
+    path('search/<location>/', views.location_search_only, name="location_search_only"),
     path('search/', views.bad_search, name="bad_search")
 ]
