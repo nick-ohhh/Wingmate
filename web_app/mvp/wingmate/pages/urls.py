2b2273671110286646e43396eaaafe2ed761359a
Rmolimock <495@holbertonschool.com>
2019-10-22 02:36:20 +0000

    new path for search without a location

--- web_app/mvp/wingmate/pages/urls.py
+++ web_app/mvp/wingmate/pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('search/<location>/', views.search, name="search")
+    path('search/<location>/', views.search, name="search"),
+    path('search/', views.bad_search, name="bad_search")
 ]
