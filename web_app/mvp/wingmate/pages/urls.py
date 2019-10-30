d31b04cb4bf4d2b2d9ea0049e7e229af6e4247bf
root <root@instance-3.us-central1-a.c.wingmates.internal>
2019-10-30 04:48:45 +0000

    added urls for ip address

--- web_app/mvp/wingmate/pages/urls.py
+++ web_app/mvp/wingmate/pages/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('search/<location>/<date>/<time>', views.full_search, name="full_search"),
-    path('search/<location>/', views.location_search_only, name="location_search_only"),
     path('search/', views.bad_search, name="bad_search")
 ]
