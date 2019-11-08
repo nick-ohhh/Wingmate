7acc8375df4aa81fd94897a7ef618c2248695062
Rmolimock <495@holbertonschool.com>
2019-11-08 19:13:07 +0000

    removed unnecessary title from admin panel page

--- web_app/mvp/wingmate/pages/admin.py
+++ web_app/mvp/wingmate/pages/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 
 # Register your models here.
 admin.site.site_header = "Wingmate Admin Panel"
-admin.site.site_title = "Yeaaa thaattss riighhttt"
 admin.site.index_title = "Wingmate!"  
