0539ac26a0bf40d44ee0b3cc360df8143ec62bfa
Rmolimock <495@holbertonschool.com>
2019-11-08 19:19:02 +0000

    undoing attempt to reorganize directories as it would delete previous commits

--- mvp/wingmate/pages/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-admin.site.site_header = "Wingmate Admin Panel"
-admin.site.index_title = "Wingmate!"  
--- mvp/wingmate/pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('search/<location>/<date>/<time>', views.full_search, name="full_search"),
-]
