76a9e3f662af08ae395598abacb55ad553979718
Rmolimock <495@holbertonschool.com>
2019-11-08 07:47:26 +0000

    removed old route for a bad request

--- web_app/mvp/wingmate/pages/urls.py
+++ web_app/mvp/wingmate/pages/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('search/<location>/<date>/<time>', views.full_search, name="full_search"),
-    path('search/', views.bad_search, name="bad_search")
 ]
